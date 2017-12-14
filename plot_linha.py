6a454f094356be42e300fe1909ac2f01281f416e
joahannes <joahannes@gmail.com>
2017-12-14 05:02:54 -0200

    Atualizacao plot_linha.py

--- plot_linha.py
+++ plot_linha.py
@@ -1,13 +1,27 @@
 # -*- coding: UTF-8 -*-
 
+# File name: plot_linha.py
+# Author: Joahannes Costa
+# Data create: 14/12/2017
+# Data last modified: 14/12/2017
+# Python version: 2.7
+# Description: plota graficos de linha para algoritmos de disseminacao
+
 from __future__ import division
 
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-# VARIABLES LOG
-# 0 REPLICATION, 1 HOST, 2 LOSS, 3 DELAY, 4 RECEIVED, 5 TRANSMITTED, 6 DUPLICATES, 7 COLLISIONS
+# summary_file:
+# 0 REPLICATION
+# 1 HOST
+# 2 LOSS
+# 3 DELAY
+# 4 RECEIVED
+# 5 TRANSMITTED
+# 6 DUPLICATES
+# 7 COLLISIONS
 
 ALGORITHMS = ("DDRX", "CARRO", "UV-CAST", "CC-DEGREE", "Flooding") #
 TRAFFICS = (100,200,300,400,500,600,700) #
@@ -17,9 +31,13 @@ MARKS = {"DDRX" : "H", "CARRO" : "^", "UV-CAST" : "d", "CC-DEGREE" : "*", "Flood
 LINES = {"DDRX" : "-", "CARRO" : "--", "UV-CAST" : "-.", "CC-DEGREE" : "-", "Flooding" : ":"}
 
 #VARIABLES PLOT
-X_LEGEND = 'Densidade (veículos/km$^2$)'.decode('utf-8')
+X_LEGEND = u'Densidade (veículos/km$^2$)'
 x_lim = 750
-formato = '.png'
+x_fig = 6.8 #6.8
+y_fig = 5.5 #5.5
+formato = '.eps'
+legenda_size = 16
+#bbox_inches = 'tight', pad_inches = 0.05 -> remove borda desnecessaria da imagem
 
 #CONFIDENCE INTERVAL - BAR ERROR
 def confidence_interval(values):
@@ -28,7 +46,7 @@ def confidence_interval(values):
 	std_dev = np.std(values)
 	std_error = std_dev / math.sqrt(N)
 	return Z * std_error
-
+	
 #COVERAGE
 def plot_coverage():
 	
@@ -46,46 +64,43 @@ def plot_coverage():
 			coverage = 0.0
 			
 			for line in open(file_input_name):
-				
 				if replication == int(line.split()[0]): 
 					if line.split()[4] != "0":
 						coverage += int(line.split()[4])
-				
+						
 				else:
-					# CHANGED
+				# CHANGED
 					if not coverage == 0:
 						values.append(coverage)
 						coverage = 0
-				
+						
 				while replication != int(line.split()[0]):
 					replication += 1
 			
-			#print(algorithm, " - values: ",values)
-			values = np.divide(values,traffic)
-			#print(algorithm, " - divide: ", np.divide(values,traffic))
-			values = np.multiply(values,100)
-			#print(algorithm, " - multiply: ", np.multiply(values,100))
+			values = np.divide(values, traffic)
+			values = np.multiply(values, 100)
 			
 			coverage_values.append(np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-		
+			
 		plt.errorbar(TRAFFICS, coverage_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2, zorder=3)
-		
-	yticks = np.arange(88, 102, 2)
-	plt.xlabel(X_LEGEND)
-	plt.ylabel('Cobertura (%)')
-	plt.xlim(50, x_lim)
-	plt.xticks(TRAFFICS)
-	plt.yticks(yticks)
 	
+	yticks = np.arange(90, 102, 2)
+	plt.xlabel(X_LEGEND, fontsize=legenda_size)
+	plt.ylabel('Cobertura (%)', fontsize=legenda_size)
+	plt.xlim(50, x_lim)
+	plt.xticks(TRAFFICS, fontsize=legenda_size)
+	plt.yticks(yticks, fontsize=legenda_size)
+
 	#plt.yscale("log")
 	
-	plt.grid() 
+	plt.grid()
 	plt.legend(numpoints=1, loc=4, fancybox=True)
 	
 	fig = plt.gcf()
-	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('cobertura' + formato, dpi=200)
+	
+	fig.set_size_inches(x_fig, y_fig)   
+	fig.savefig('cobertura' + formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 	
 	plt.show()
 	
@@ -106,39 +121,34 @@ def plot_delay():
 			delay = 0.0
 			
 			for line in open(file_input_name):
-				
 				if replication == int(line.split()[0]): 
 					if line.split()[3] != "NA":
 						delay += float(line.split()[3])
-					
+			
 				else:
 				# CHANGED
 					if not delay == 0.0:
 						values.append(delay)
 						delay = 0.0
-					
+				
 				while replication != int(line.split()[0]):
 					replication += 1
-				
-			#print(algorithm, " - values: ",values)
+			
 			#values = np.divide(values,traffic)
-			#print(algorithm, " - divide: ", np.divide(values,traffic))
 			#values = np.multiply(values,60)
-			#print(algorithm, " - multiply: ", np.multiply(values,60))
-				
+			
 			delay_values.append(np.mean(values))
-			#print(algorithm, " - mean:", np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(algorithm, " - confidence: ", confidence_interval(values))
 		
+		#print (algorithm, delay_values)	
 		plt.errorbar(TRAFFICS, delay_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2, zorder=3)
-		
-	#yticks = np.arange(50, 110, 10)
-	plt.xlabel(X_LEGEND)
-	plt.ylabel('Atraso (s)')
+	
+	yticks = np.arange(0, 110, 10)
+	plt.xlabel(X_LEGEND, fontsize=legenda_size)
+	plt.ylabel('Atraso (s)', fontsize=legenda_size)
 	plt.xlim(50, x_lim)
-	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
+	plt.xticks(TRAFFICS, fontsize=legenda_size)
+	plt.yticks(yticks, fontsize=legenda_size)
 	
 	plt.yscale("log")
 	
@@ -146,8 +156,8 @@ def plot_delay():
 	plt.legend(numpoints=1, loc=2, fancybox=True)
 	
 	fig = plt.gcf()
-	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('atraso' + formato, dpi=200)
+	fig.set_size_inches(x_fig, y_fig)
+	fig.savefig('atraso' + formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 	
 	plt.show()
 	
@@ -175,39 +185,34 @@ def plot_transmitted():
 					if not transmitted == 0:
 						values.append(transmitted)
 						transmitted = 0
-				
+						
 				while replication != int(line.split()[0]):
 					replication += 1
 			
-			#print(algorithm, " - values: ",values)
 			#values = np.divide(values,traffic)
-			#print(algorithm, " - divide: ", np.divide(values,traffic))
 			#values = np.multiply(values,100)
-			#print(algorithm, " - multiply: ", np.multiply(values,100))
 			
 			transmitted_values.append(np.mean(values))
-			#print(algorithm, " - soma:", np.sum(transmitted_values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(algorithm, " - confidence: ", confidence_interval(values))
 		
+		#print (algorithm, transmitted_values)
 		plt.errorbar(TRAFFICS, transmitted_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2, zorder=3)
-		
-	#yticks = np.arange(50, 110, 10)
-	plt.xlabel(X_LEGEND)
-	plt.ylabel('Total de Pacotes Transmitidos')
+	
+	yticks = np.arange(0, 110, 10)
+	plt.xlabel(X_LEGEND, fontsize=legenda_size)
+	plt.ylabel('Total de Pacotes Transmitidos', fontsize=legenda_size)
 	plt.xlim(50, x_lim)
-	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
+	plt.xticks(TRAFFICS, fontsize=legenda_size)
+	plt.yticks(yticks, fontsize=legenda_size)
 	
 	plt.yscale("log")
 	
 	plt.grid()
-	
 	plt.legend(numpoints=1, loc=2, fancybox=True)
 	
 	fig = plt.gcf()
-	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('transmitidos' + formato, dpi=200)
+	fig.set_size_inches(x_fig, y_fig)
+	fig.savefig('transmitidos' + formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 	
 	plt.show()
 	
@@ -239,25 +244,21 @@ def plot_colisions():
 				while replication != int(line.split()[0]):
 					replication += 1
 			
-			#print(algorithm, " - values: ",values)
 			#values = np.divide(values,traffic)
-			#print(algorithm, " - divide: ", np.divide(values,traffic))
 			#values = np.multiply(values,100)
-			#print(algorithm, " - multiply: ", np.multiply(values,100))
 			
 			colisions_values.append(np.mean(values))
-			#print(algorithm, " - mean:", np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(algorithm, " - confidence: ", confidence_interval(values))
 		
+		#print (algorithm, colisions_values)
 		plt.errorbar(TRAFFICS, colisions_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2, zorder=3)
-		
-	#yticks = np.arange(0, 11, 1)
-	plt.xlabel(X_LEGEND)
-	plt.ylabel('Número de Colisões'.decode('utf-8'))
+	
+	yticks = np.arange(0, 20, 1)
+	plt.xlabel(X_LEGEND, fontsize=legenda_size)
+	plt.ylabel(u'Número de Colisões', fontsize=legenda_size)
 	plt.xlim(50, x_lim)
-	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
+	plt.xticks(TRAFFICS, fontsize=legenda_size)
+	plt.yticks(yticks, fontsize=legenda_size)
 	
 	plt.yscale("log")
 	
@@ -265,13 +266,14 @@ def plot_colisions():
 	plt.legend(numpoints=1, loc=2, fancybox=True)
 	
 	fig = plt.gcf()
-	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('colisoes' + formato, dpi=200)
+	fig.set_size_inches(x_fig, y_fig)
+	fig.savefig('colisoes' + formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 	
 	plt.show()
 	
 if __name__ == "__main__":
 	pass
+	
 	print("Grafico de COBERTURA...")
 	plot_coverage()
 	
